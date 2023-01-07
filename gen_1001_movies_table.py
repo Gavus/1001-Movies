4f40168252c7db7c228cdda84cbe9d7e83197a28
Gustav Johansson <gustavsj@outlook.com>
2023-01-07 18:33:43 +0100

    Add list-not-watched and rand-not-watched

--- gen_1001_movies_table.py
+++ gen_1001_movies_table.py
@@ -4,6 +4,7 @@ Generate a markdown table based of movies.txt
 """
 
 from argparse import ArgumentParser
+from random import randint
 from numpy import nan
 from pandas import DataFrame, read_csv
 
@@ -27,10 +28,32 @@ def save_csv(dataframe: DataFrame, csv: str):
 
 
 def print_info(dataframe: DataFrame):
+    "Print number of movies watched."
+
     nbr_titles = len(dataframe['Title'])
     nbr_watched = sum(x != "" for x in dataframe['Watched'])
     percentage = f"{100*nbr_watched/nbr_titles:.2f}%"
     print(f"Total movies watched: {nbr_watched}/{nbr_titles} {percentage}")
+
+
+def get_not_watched(dataframe: DataFrame):
+    "Print all movies not watched."
+
+    res = dataframe.loc[(dataframe['Watched'] == '')]
+    for i in res.index:
+        year = dataframe['Year'][i]
+        title = dataframe['Title'][i]
+        print(f"{year} {title}")
+
+
+def rand_not_watched(dataframe: DataFrame):
+    "Randomly choose a movie not watched."
+
+    res = dataframe.loc[(dataframe['Watched'] == '')]
+    rand_i = randint(0, len(res.index))
+    year = dataframe['Year'][rand_i]
+    title = dataframe['Title'][rand_i]
+    print(f"{year} {title}")
 
 
 def save_markdown(dataframe: DataFrame, markdown: str):
@@ -47,23 +70,32 @@ def save_markdown(dataframe: DataFrame, markdown: str):
 def main():
     "Main wrapper function."
 
+    cmds = [
+        "save-csv", "save-table", "info", "list-not-watched",
+        "rand-not-watched"
+    ]
+
     parser = ArgumentParser(
         prog="Generate1001MoviesTable",
         description="Generate a markdown table based of movies.txt",
         epilog="text at the bottom of help")
-    parser.add_argument("command",
-                        help="What to do",
-                        choices=["save-csv", "save-table", "info"])
+    parser.add_argument("command", help="What to do", choices=cmds)
     args = parser.parse_args()
 
     dataframe = load_csv(CSVFILE)
 
     if args.command == "save-csv":
         save_csv(dataframe, CSVFILE)
-    if args.command == "save-table":
+    elif args.command == "save-table":
         save_markdown(dataframe, TABLEFILE)
-    if args.command == "info":
+    elif args.command == "info":
         print_info(dataframe)
+    elif args.command == "list-not-watched":
+        get_not_watched(dataframe)
+    elif args.command == "rand-not-watched":
+        rand_not_watched(dataframe)
+    else:
+        print("Error: Invalid option?")
 
 
 if __name__ == "__main__":
